4f9e76b34c5339a7374a79e454a8c6baffd8ce0e
zerodeleo <julia.stjerna@hotmail.se>
2024-02-02 21:58:21 +0100

    Rename about route to movies
    This commit updates the route previously named about to movies for improved clarity and consistency within the application. The change aligns with the route's purpose of displaying information related to movies.
    
    Signed-off-by: Julia Stjerna <zerodeleo@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@ import { RouterLink, RouterView } from 'vue-router'
     <div class="wrapper">
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/movies">Movies</RouterLink>
       </nav>
     </div>
   </header>
