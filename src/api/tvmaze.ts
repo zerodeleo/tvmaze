76e28ba8e3f8b9131939a2a17c06cc11c3a29f1d
zerodeleo <julia.stjerna@hotmail.se>
2024-02-02 22:37:00 +0100

    Add return type to getMovies query function
    This commit adds a return type to the getMovies query function, specifying the type of data returned from the API endpoint. By defining a clear return type, the function's signature becomes more explicit, enhancing code readability and type safety.
    
    Signed-off-by: Julia Stjerna <zerodeleo@gmail.com>

--- src/api/tvmaze.ts
+++ src/api/tvmaze.ts
@@ -1,4 +1,6 @@
-export const getMovies = async (page = 1) => {
+import type { Movies } from '@/interface/tvmaze'
+
+export const getMovies = async (page = 1): Promise<Movies> => {
   const result = await fetch(`https://api.tvmaze.com/shows?page=${page}`)
   const data = await result.json()
   return data
