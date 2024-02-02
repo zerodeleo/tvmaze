df168494f022ae935342d883bbc078533d02e74f
zerodeleo <julia.stjerna@hotmail.se>
2024-02-02 22:10:33 +0100

    Initialize Query Client
    This commit initializes the Query Client, an essential step for using TanStack Vue Query in the project. The Query Client is configured to manage data fetching and caching, providing a centralized mechanism for handling asynchronous data operations within the Vue.js application.
    
    Signed-off-by: Julia Stjerna <zerodeleo@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,6 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { VueQueryPlugin } from '@tanstack/vue-query'
 
-const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
+createApp(App).use(router).use(VueQueryPlugin).mount('#app')
