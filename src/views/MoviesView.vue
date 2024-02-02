2595db062cfd54aad05964feb5ddc8e72428effd
zerodeleo <julia.stjerna@hotmail.se>
2024-02-02 22:38:45 +0100

    Add dynamic loading of movies using TanStack Vue Query
    This commit introduces dynamic loading of movies using TanStack Vue Query. The component template is updated to conditionally render loading, error, or movie data based on the query status. The useQuery hook fetches movies from the API endpoint with the specified query options. Additionally, TypeScript is leveraged to ensure type safety throughout the implementation.
    
    Signed-off-by: Julia Stjerna <zerodeleo@gmail.com>

--- src/views/MoviesView.vue
+++ src/views/MoviesView.vue
@@ -1,5 +1,25 @@
 <template>
-  <div class="movies">
-    <h1>This is the movies page</h1>
-  </div>
+    <span v-if="isLoading">Loading...</span>
+    <span v-else-if="isError">Error: {{ error?.message }}</span>
+    <ul v-else-if="data">
+      <li v-for="movie in data" :key="movie.id">{{ movie.name }}</li>
+    </ul>
 </template>
+
+<script setup lang="ts">
+import { useQuery } from '@tanstack/vue-query'
+import type { UseQueryOptions, QueryKey } from '@tanstack/vue-query'
+import { getMovies } from '@/api/tvmaze'
+import type { Movies } from '@/interface/tvmaze'
+import { ref } from 'vue';
+
+const currentPage = ref(1);
+
+const options: UseQueryOptions<Movies, Error, Movies, Movies, QueryKey> = {
+  queryKey: ['movies'],
+  queryFn: () => getMovies(currentPage.value),
+};
+
+const { isLoading, isError, data, error } = useQuery(options);
+
+</script>
