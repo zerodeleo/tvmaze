cb3076130508f4b6ec9716bacbb699a0c7284a87
zerodeleo <julia.stjerna@hotmail.se>
2024-02-02 22:12:41 +0100

    Add interfaces and types for API response data
    This commit introduces interfaces and types to define the structure of the data returned from the API responses. By defining clear interfaces and types, the project gains better type safety and improved code readability. This step enhances development efficiency and reduces the likelihood of runtime errors related to data handling.
    
    Signed-off-by: Julia Stjerna <zerodeleo@gmail.com>

---
+++ src/interface/tvmaze.ts
@@ -0,0 +1,91 @@
+export type Movies = Movie[]
+
+export interface Movie {
+  id: number
+  url: string
+  name: string
+  type: string
+  language: string
+  genres: string[]
+  status: string
+  runtime?: number
+  averageRuntime?: number
+  premiered?: string
+  ended?: string
+  officialSite?: string
+  schedule: Schedule
+  rating: Rating
+  weight: number
+  network?: Network
+  webChannel?: WebChannel
+  dvdCountry: any
+  externals: Externals
+  image: Image
+  summary: string
+  updated: number
+  _links: Links
+}
+
+export interface Schedule {
+  time: string
+  days: string[]
+}
+
+export interface Rating {
+  average?: number
+}
+
+export interface Network {
+  id: number
+  name: string
+  country: Country
+  officialSite?: string
+}
+
+export interface Country {
+  name: string
+  code: string
+  timezone: string
+}
+
+export interface WebChannel {
+  id: number
+  name: string
+  country?: Country2
+  officialSite?: string
+}
+
+export interface Country2 {
+  name: string
+  code: string
+  timezone: string
+}
+
+export interface Externals {
+  tvrage: number
+  thetvdb?: number
+  imdb?: string
+}
+
+export interface Image {
+  medium: string
+  original: string
+}
+
+export interface Links {
+  self: Self
+  previousepisode?: Previousepisode
+  nextepisode?: Nextepisode
+}
+
+export interface Self {
+  href: string
+}
+
+export interface Previousepisode {
+  href: string
+}
+
+export interface Nextepisode {
+  href: string
+}
