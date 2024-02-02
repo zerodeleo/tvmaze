902da6615eb1c198325f32532e1bfb258e8fe92b
zerodeleo <julia.stjerna@hotmail.se>
2024-02-02 23:31:56 +0100

    Refactor movie list pagination logic
    This commit refactors the movie list component to include pagination functionality. The updated component now displays a list of movies with pagination controls, allowing users to navigate between pages. Additionally, the code has been optimized to compute the start and end indexes for pagination and display the appropriate subset of movies on each page.
    
    Signed-off-by: Julia Stjerna <zerodeleo@gmail.com>

--- src/views/MoviesView.vue
+++ src/views/MoviesView.vue
@@ -1,9 +1,13 @@
 <template>
-    <span v-if="isLoading">Loading...</span>
-    <span v-else-if="isError">Error: {{ error?.message }}</span>
-    <ul v-else-if="data">
-      <li v-for="movie in data" :key="movie.id">{{ movie.name }}</li>
-    </ul>
+  <span v-if="isLoading">Loading...</span>
+  <span v-else-if="isError">Error: {{ error?.message }}</span>
+  <ul v-else-if="data">
+    <p>{{ currentPage }}</p>
+    <p>{{ totalPages }}</p>
+    <button @click="goToPage(currentPage - 1)" :disabled="currentPage === 1">Previous Page</button>
+    <button @click="goToPage(currentPage + 1)" :disabled="currentPage === totalPages">Next Page</button>
+    <li v-for="movie in paginatedData" :key="movie.id">{{ movie.name }}</li>
+  </ul>
 </template>
 
 <script setup lang="ts">
@@ -11,9 +15,20 @@ import { useQuery } from '@tanstack/vue-query'
 import type { UseQueryOptions, QueryKey } from '@tanstack/vue-query'
 import { getMovies } from '@/api/tvmaze'
 import type { Movies } from '@/interface/tvmaze'
-import { ref } from 'vue';
+import { computed, ref, watch } from 'vue';
+
+const getStartIndex = () => (currentPage.value - 1) * moviesPerPage.value;
+const getEndIndex = () => startIndex.value + moviesPerPage.value;
+const getPaginatedData = () => data.value!.slice(startIndex.value, endIndex.value)
+const getTotalPages = () => Math.ceil(data.value!.length / moviesPerPage.value)
 
 const page = ref(1);
+const moviesPerPage = ref(10);
+const totalPages = computed(getTotalPages)
+const currentPage = ref(1);
+const startIndex = computed(getStartIndex);
+const endIndex = computed(getEndIndex);
+const paginatedData = computed<Movies>(getPaginatedData);
 
 const options: UseQueryOptions<Movies, Error, Movies, Movies, QueryKey> = {
   queryKey: ['movies', page],
@@ -21,5 +36,16 @@ const options: UseQueryOptions<Movies, Error, Movies, Movies, QueryKey> = {
 };
 
 const { isLoading, isError, data, error } = useQuery(options);
+
+const goToPage = (page: number) => {
+  currentPage.value = page;
+}
+
+watch(data, () => {
+  totalPages.value;
+  startIndex.value;
+  endIndex.value;
+  paginatedData.value;
+});
 
 </script>
